3843b210a18adc748505c39c652dd0971905385d
Amio <amio.cn@gmail.com>
2014-05-17 22:31:23 +0800

    Add method "register" to Console instances.

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
+window.onload = function () {
 
-window.onload = function(){
   var cnsl = new Console({
-    "addbots": function(num){
+    "addbots": function (num) {
       return num + ' bots added.'
     }
   });
+
+  cnsl.register('setname', function (name) {
+    return 'My name is ' + name + ' now.';
+  }, {
+    desc: 'Change your name (works in network play too).'
+  });
+
+  cnsl.register('whosyourdaddy', function () {
+    console.log('WHOSYOURDADDY!');
+  });
+
+  cnsl.register('help', function () {
+    var cmds = cnsl.commands;
+    for (var name in cmds) {
+      if (cmds.hasOwnProperty(name)) {
+        cmds[name].desc && cnsl.log(' -', name.toUpperCase() + ':', cmds[name].desc);
+      }
+    }
+  },{
+    desc: 'Show help messages.'
+  });
+
+  window.cnsl = cnsl;
 };
