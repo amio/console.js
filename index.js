16bc63dcfe2a2698b2c9f572ef7f9396b1e045fc
Amio <amio.cn@gmail.com>
2014-05-25 20:02:42 +0800

    Tidy code.

--- index.js
+++ index.js
@@ -1,42 +1,56 @@
 window.onload = function () {
 
-  var player = {
-    name: 'Player'
-  };
-
-  var cnsl = new Console({
-    name: 'setname' // Alias Command
-  }, {
+  // Init Console
+  var cnsl = new Console({}, {
     hotKey: 27, // <kbd>ESC</kbd>
     welcome: 'Try "help":'
   });
 
-  cnsl.register('say', function (something) {
-    return player.name + ': "' + something + '"';
-  }, {
-    usage: 'SAY &lt;message string&gt;',
-    desc: 'Broadcast a message to other players in the game.'
-  });
+  // Game data
+  var player = {
+    name: 'Player'
+  };
 
-  cnsl.register('setname', function (name) {
-    player.name = name;
-    return 'Player name is ' + player.name + ' now.';
-  }, {
-    usage: 'SETNAME &lt;newname&gt; || NAME &lt;newname&gt;',
-    desc: 'Change your name (works in network play too).'
-  });
+  /**
+   * Register console command handlers.
+   */
 
-  cnsl.register('help', function () {
-    var cmds = cnsl.commands;
-    for (var name in cmds) {
-      if (cmds.hasOwnProperty(name)) {
-        cmds[name].desc && cnsl.log(' -', cmds[name].usage + ':', cmds[name].desc);
+  var handlers = {
+    say: function (something) {
+      return player.name + ': "' + something + '"';
+    },
+    name: 'setname',
+    setname: function (name) {
+      player.name = name;
+      return 'Player name is ' + player.name + ' now.';
+    },
+    help: function () {
+      var cmds = cnsl.commands;
+      for (var name in cmds) {
+        if (cmds.hasOwnProperty(name)) {
+          cmds[name].desc && cnsl.log(' -', cmds[name].usage + ':', cmds[name].desc);
+        }
       }
     }
-  }, {
-    usage: 'HELP',
-    desc: 'Show help messages.'
-  });
+  };
 
-  window.cnsl = cnsl;
+  var handlerProps = {
+    say: {
+      usage: 'SAY &lt;message string&gt;',
+      desc: 'Broadcast a message to other players in the game.'
+    },
+    setname: {
+      usage: 'SETNAME &lt;newname&gt; || NAME &lt;newname&gt;',
+      desc: 'Change your name (works in network play too).'
+    },
+    help: {
+      usage: 'HELP',
+      desc: 'Show help messages.'
+    }
+  };
+
+  for (var cmdname in handlers) {
+    cnsl.register(cmdname, handlers[cmdname], handlerProps[cmdname]);
+  }
+
 };
