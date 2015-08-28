bff0508bfd86d7d3fb32d33e536a0d44c9144482
Amio <amio.cn@gmail.com>
2015-08-28 13:40:37 +0800

    (chore) add `gulp watch`

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,18 @@ var cssholder = '/* CONSOLE CSS */';
 
 gulp.task('build', function(){
   gulp.src('lib/console.js')
-  .pipe(replace(cssholder, new CleanCSS().minify(fs.readFileSync(cssfile)).styles))
+  .pipe(replace(
+    cssholder,
+    new CleanCSS().minify(fs.readFileSync(cssfile)).styles)
+  )
   .pipe(uglify())
   .pipe(rename('console.min.js'))
   .pipe(gulp.dest('./lib/'));
 });
+
+gulp.task('watch', function(){
+  gulp.watch([
+    'lib/console.css',
+    'lib/console.js'
+  ], ['build'])
+})
