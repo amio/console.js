00b545fb2127ab80f8f24831d21a8f8bf56dba05
Amio <amio.cn@gmail.com>
2017-02-20 12:45:23 +0800

    Updates

--- index.js
+++ index.js
@@ -1,65 +1,54 @@
 window.onload = function () {
-  // Init Console
-  var cnsl = new window.Console({}, {
-    welcome: 'Hello user. Need any "help"?'
+  /**
+   * Init Console 1: cnsl
+   */
+
+  var cnsl = new window.Console({
+    welcome: 'Hello Player. Need any help?'
   })
 
-  // Game data
+  // Virtual game data
   var player = {
     name: 'Player'
   }
 
+  // .register(<name>, <handler>, <anything>)
+  cnsl.register('say', function () {
+    return player.name + ': "' + Array.prototype.join.call(arguments, ' ') + '"'
+  }, {
+    usage: 'SAY <message string>: Broadcast a message to other players in the game.'
+  })
+
+  cnsl.register('setname', function (name) {
+    player.name = name
+    return 'Player name is ' + player.name + ' now.'
+  }, {
+    usage: 'SETNAME <newname>; || NAME <newname>: Change your name (works in network play too).'
+  })
+
+  cnsl.register('help', function () {
+    return Object.keys(cnsl.handlers).map(function (name) {
+      return ' - ' + cnsl.handlers[name].cfg.usage
+    }).join('\n')
+  }, {
+    usage: 'HELP: Show help messages.'
+  })
+
+  cnsl.register(function () {
+    return 'Unrecognized command. Use `help`.'
+  })
+
   /**
-   * Register console command handlers.
+   *  Init Console 2: Show Me The Code
    */
-
-  var handlers = {
-    say: function () {
-      return player.name + ': "' + Array.prototype.join.call(arguments, ' ') + '"'
-    },
-    name: 'setname',
-    setname: function (name) {
-      player.name = name
-      return 'Player name is ' + player.name + ' now.'
-    },
-    help: function () {
-      var cmds = cnsl.commands
-      for (var name in cmds) {
-        if (cmds.hasOwnProperty(name)) {
-          cmds[name].desc && cnsl.log(' -', cmds[name].usage + ':', cmds[name].desc)
-        }
-      }
-    }
-  }
-
-  var handlerProps = {
-    say: {
-      usage: 'SAY &lt;message string&gt;',
-      desc: 'Broadcast a message to other players in the game.'
-    },
-    setname: {
-      usage: 'SETNAME &lt;newname&gt; || NAME &lt;newname&gt;',
-      desc: 'Change your name (works in network play too).'
-    },
-    help: {
-      usage: 'HELP',
-      desc: 'Show help messages.'
-    }
-  }
-
-  for (var cmdname in handlers) {
-    cnsl.register(cmdname, handlers[cmdname], handlerProps[cmdname])
-  }
-
-  // Init Console 2
 
   var codes = {
     'showmethecode':
-      ' - createconsole: How to init a new console.\n' +
+      ' - create: How to init a new console.\n' +
       ' - options: Available options.\n' +
       ' - more: More docs.',
     'create':
-      'new Console({\n' +
+      'new Console({ hotkey: 27 }, {\n' +
       '  "addbots": function (num) {\n' +
       '      // add some bots,\n' +
       '      // then tell player:\n' +
@@ -67,7 +56,7 @@ window.onload = function () {
       '  }\n' +
       '});',
     'options':
-      'new Console({}, {\n' +
+      'new Console({\n' +
       '    hotkey: 27, \n' +
       '    welcome: "Hello User:",\n' +
       '    caseSensitive: true,\n' +
@@ -75,17 +64,16 @@ window.onload = function () {
       '    onShow: function(){},\n' +
       '    onHide: function(){}\n' +
       '});\n' +
-      ' - `hotkey` : {Number|boolean} The keyCode of hotkey. *Hint: If you want to manually put' +
-      ' up console(`cnsl.toggle("on")`), set to a falsy value.* `192` by default, the "~".\n' +
+      ' - `hotkey` : {Number|boolean} The keyCode of hotkey. `192`(the "~") by default.\n' +
       ' - `welcome`: {String} The welcome message. `""` by default.\n' +
       ' - `caseSensitive`: {Boolean} If you want to. `false` by default.' +
-      ' - `defaultHandler`: {Function} the default handler for any unspecified command. `null` by default.\n' +
-      ' - `onShow` : {Function} On show callback. `null` by default.\n' +
-      ' - `onHide` : {Function} On hide callback. `null` by default.',
-    'more': 'Visit <a href="http://github.com/amio/console.js/">http://github.com/amio/console.js/</a>'
+      ' - `defaultHandler`: {Function} the fallback handler for all commands. `noop` by default.\n' +
+      ' - `onShow` : {Function} On show callback. `noop` by default.\n' +
+      ' - `onHide` : {Function} On hide callback. `noop` by default.',
+    'more': 'Visit http://github.com/amio/console.js/'
   }
 
-  var smtc = new window.Console({}, {
+  var smtc = new window.Console({
     hotkey: 27, // <kbd>ESC</kbd>
     welcome: 'Use "showmethecode":',
     caseSensitive: true,
@@ -102,4 +90,7 @@ window.onload = function () {
   // For tablet visiters
   document.getElementById('slash').addEventListener('click', function () { cnsl.toggle() })
   document.getElementById('esc').addEventListener('click', function () { smtc.toggle() })
+
+  window.cnsl = cnsl
+  window.smtc = smtc
 }
